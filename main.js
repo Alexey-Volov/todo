f3a59af2f93a9a86cd20d0ed6596d36c266809ac
Alexey <alexey.verstka@gmail.com>
2023-01-07 14:22:05 +0300

    add task

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
 const btn = document.querySelector("btn")
 const input = document.querySelector("input")
+const btnDel = document.querySelector("#btnDel")
+const form = document.querySelector("#form")
+const taskWrapper = document.querySelector("#tasks")
 
+form.addEventListener("click", addTask)
+
+function addTask(e) {
+    e.preventDefault()
+    const correctBtn = e.target.dataset.btn
+    const htmlTask = `
+    <li class="task">
+        <p class="task__text">${input.value}</p>
+        <button class="btnDel" id="btnDelete">Delete Task</button>
+    </li>`
+    if(correctBtn) {
+        if(input.value) {
+            taskWrapper.insertAdjacentHTML("beforeend", htmlTask)
+            console.log(true)
+            clearInput()
+            input.focus()
+        }
+        else {
+            console.log(false)
+        }
+    }
+}
+function clearInput() {
+    input.value = "";
+}
